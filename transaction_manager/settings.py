1f7770f77d0f95c6eca52142de04aa0b80f0ee16
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2023-05-26 21:03:09 +0530

    Update settings.py

--- transaction_manager/settings.py
+++ transaction_manager/settings.py
@@ -60,7 +60,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
