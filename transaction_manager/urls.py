a286fcb08aaec4763f33fdd5b50f34a0e9fc23b9
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2023-05-26 14:44:24 +0530

    version 1.6 ready - added admin static files

--- transaction_manager/urls.py
+++ transaction_manager/urls.py
@@ -7,3 +7,17 @@ urlpatterns = [
     path('', include("group.urls")),
     
 ]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(
+                settings.MEDIA_URL,
+                document_root = settings.MEDIA_ROOT
+            )
+
+urlpatterns += static(
+                settings.STATIC_URL,
+                document_root = settings.STATIC_ROOT
+            )
