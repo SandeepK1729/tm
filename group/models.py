a366d573afb28205bcc6c4e4080c5df3c7f8e5df
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2023-06-16 16:09:51 +0530

    mend

--- group/models.py
+++ group/models.py
@@ -1,29 +1,35 @@
-from django.db import models
+from django.db                  import models
 
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
-
-from datetime import date
-from django.utils import timezone
+from django.utils               import timezone
+from django.utils.translation   import gettext_lazy as _
+from django.utils               import timezone
 
 
 class Group(models.Model):
-    name    = models.CharField(
-                _("Group Name"),
-                unique = True,
-                error_messages={
-                    "unique": _("A group with that groupname already exists."),
-                },
-                max_length = 30,
-            )
-    created_by = models.ForeignKey("core.User", on_delete = models.CASCADE)
-    created_on = models.DateTimeField(default = timezone.now)
+    """
+        Group model
+    """
+    name        = models.CharField(
+                    _("Group Name"),
+                    unique = True,
+                    error_messages={
+                        "unique": _("A group with that groupname already exists."),
+                    },
+                    max_length = 30,
+                )
+    created_by  = models.ForeignKey("core.User", on_delete = models.CASCADE)
+    created_on  = models.DateTimeField(default = timezone.now)
 
     def __str__(self):
         return f"{self.name} created by {self.created_by}"
 
     @property
     def get_members(self):
+        """get all members associated with group
+
+        Returns:
+            list[User Object]: list of users
+        """
         return [x for x in self.members.all().order_by('id')]
     
     def save(self, *args, **kwargs):
@@ -43,13 +49,13 @@ class Transaction(models.Model):
     """
         Transaction Model
     """
-    transaction_for = models.CharField(verbose_name = "for", max_length = 60) #, unique_for_date = True)
+    transaction_for = models.CharField(verbose_name = "for", max_length = 60) 
     by              = models.ForeignKey("core.User", on_delete = models.CASCADE)
-    to              = models.CharField(verbose_name = "to" , max_length = 60) #, unique_for_date = True)
+    to              = models.CharField(verbose_name = "to" , max_length = 60) 
     amount          = models.IntegerField()
     of_group        = models.ForeignKey(Group, on_delete = models.CASCADE, related_name = "transactions")
     on              = models.DateField(default = timezone.now)
-    
+    # added           = models.ForeignKey("core.User", on_delete = models.CASCADE) 
 
     def __str__(self):
         return f"transaction for {self.transaction_for} by {self.by} on {self.on} to {self.to} of amount {self.amount}"
--- transaction_manager/settings.py
+++ transaction_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure--7-$xg9#$m!s#+x!0$q^vl1uv=*yajot=p0j20s(nt)b7wq969
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default = True)
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = [
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
