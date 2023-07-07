f97e8d7e49d1bce758fc18b665655b0cb8ef3b46
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2023-07-07 16:08:34 +0530

    fix static assest issue #14 logo add

--- transaction_manager/urls.py
+++ transaction_manager/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin', admin.site.urls),
@@ -8,16 +8,10 @@ urlpatterns = [
     
 ]
 
-
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import serve
 
-urlpatterns += static(
-                settings.MEDIA_URL,
-                document_root = settings.MEDIA_ROOT
-            )
-
-urlpatterns += static(
-                settings.STATIC_URL,
-                document_root = settings.STATIC_ROOT
-            )
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+]
